81887ea46883b619cbaf4b64e630e497034dc114
arodriguez133 <arodriguez133@icloud.com>
2022-03-07 22:57:19 -0600

    wip

--- api/server.js
+++ api/server.js
@@ -3,6 +3,11 @@ const express = require('express');
 const users = require('./users/model');
 const server = express();
 
+server.get('/api/users', (req, res)=> {
+    console.log("Getting all users");
+    res.send('working');
+});
+
 server.use('*', (req, res)=> {
     res.status(404).json({message: "not found"});
 });
