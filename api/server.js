1eee13f73e4de09e0d2da3de5cc12c1936061f6e
arodriguez133 <arodriguez133@icloud.com>
2022-03-08 00:12:10 -0600

    finished all operations

--- api/server.js
+++ api/server.js
@@ -56,6 +56,51 @@ server.post('/api/users', (req,res)=>{
     }
 })
 
+server.delete('/api/users/:id', async (req, res)=> {
+    const {id} = req.params
+    const possibleUser = await User.findById(id);
+    if(!possibleUser){
+        res.status(404).json({
+            message: "The user with the specified ID does not exist",
+        })
+    }else{
+        const deletedUser = await User.remove(id);
+        res.status(200).json(deletedUser)
+    }
+})
+
+server.put('/api/users/:id', async (req,res)=> {
+    const {id} = req.params
+    const {name, bio} = req.body;
+ try{
+    const possibleUser =  await User.findById(id);
+    if(!possibleUser){
+        res.status(404).json({
+            message: "The user with the specified ID does not exist",
+        })
+    }else{
+        if(!name || !bio){
+            res.status(400).json({
+                message: "Please provide name and bio for user",
+            })
+        }else{
+           const updatedUser = await User.update(
+               req.params.id, 
+               req.body
+        );
+           res.status(200).json(updatedUser);
+        }
+    } 
+ }
+ catch(error){
+     res.status(500).json({
+         message: "Error updating user",
+         error: error.message,
+         stack: error.stack,
+     })
+ }
+})
+
 server.use('*', (req, res)=> {
     res.status(404).json({message: "not found"});
 });
